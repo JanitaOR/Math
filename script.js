a0649bbe49d9c3d4b7292505bfc988b57422ba9a
Janita Omdahl Reiraa <janitaomdahlreiraa@gmail.com>
2024-12-16 15:52:16 +0100

    omgjode

--- script.js
+++ script.js
@@ -1,60 +1,6 @@
 
+
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-function generateProblem() {
-    const operation = document.getElementById('operation').value;
-    const difficulty = parseInt(document.getElementById('difficulty').value);
-    const max = Math.pow(10, difficulty);
-    const num1 = getRandomInt(max);
-    const num2 = getRandomInt(max);
-
-    const problem = `${num1} ${operation} ${num2}`;
-    document.getElementById('problem').innerText = problem;
-    document.getElementById('result').innerText = '';
-}
-
-// Lytte etter Enter-tasten på input-feltet
-document.getElementById('answer').addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        checkAnswer();
-    }
-});
-
-function checkAnswer(event) {
-    if (event) {
-        event.preventDefault();
-    }
-
-    const problem = document.getElementById('problem').innerText;
-    const answer = parseInt(document.getElementById('answer').value);
-
-    const correctAnswer = eval(problem);
-
-    if (answer === correctAnswer) {
-        document.getElementById('result').innerText = 'Riktig svar! Fortsett til neste.';
-        generateProblem();
-        document.getElementById('answer').value = '';
-    } else {
-        document.getElementById('result').innerText = 'Galt svar. Prøv igjen.';
-    }
-
-    // Sett fokus tilbake til input-feltet med en liten forsinkelse
-    setTimeout(() => {
-        document.getElementById('answer').focus();
-    }, 10);
-}
-
-// Simulerer et klikk på "Sjekk svar"-knappen uten å miste fokus
-document.getElementById('answer').addEventListener('blur', function(event) {
-    setTimeout(() => {
-        document.getElementById('answer').focus();
-    }, 0);
-});
-
-/*function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
@@ -79,7 +25,7 @@ function checkAnswer(event) {
     else { document.getElementById('result').innerText = 'Galt svar. Prøv igjen.'; } 
     // Sett fokus tilbake til input-feltet med en liten forsinkelse 
     setTimeout(() => { document.getElementById('answer').focus(); }, 10);
-}*/
+}
     /*function checkAnswer() {
     const problem = document.getElementById('problem').innerText;
     const answer = parseInt(document.getElementById('answer').value);
